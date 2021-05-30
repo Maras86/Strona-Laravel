e07b9acd22906ffda4e5837655fc3ff4ba943658
Maras86 <emil.cichocki@wp.pl>
2021-05-30 02:04:30 +0200

    Init

---
+++ resources/lang/pl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Podany login i hasło nie pasują do siebie.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Zbyt dużo prób logowania. Spróbuj ponownie za :seconds sekund.',
+];
